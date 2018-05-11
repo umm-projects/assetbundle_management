ba36e0df71b68f034fa015856c348270925dd66e
Tetsuya Mori <monry84@gmail.com>
2018-05-11 15:13:41 +0900

    :sparkles: Tweak IDownloadableProjectContext interface

--- Assets/Scripts/UnityModule/AssetBundleManagement/DownloadableProjectContext.cs
+++ Assets/Scripts/UnityModule/AssetBundleManagement/DownloadableProjectContext.cs
@@ -1,42 +1,45 @@
 ﻿using System;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityModule.ContextManagement;
 
 #pragma warning disable 649
 
-namespace UnityModule.AssetBundleManagement {
-
-    public interface IDownloadableProjectContext : IProjectContext {
-
+namespace UnityModule.AssetBundleManagement
+{
+    [PublicAPI]
+    public interface IDownloadableProjectContext : IProjectContext
+    {
         int AssetBundleSingleManifestVersion { get; }
 
-        string InitialSceneName { get; set; }
+        string InitialSceneName { get; }
 
-        IURLResolver AssetBundleURLResolverSingleManifest { get; set; }
+        IURLResolver AssetBundleURLResolverSingleManifest { get; }
 
-        IURLResolver AssetBundleURLResolver { get; set; }
-
+        IURLResolver AssetBundleURLResolver { get; }
     }
 
     // XXX: VersionResolver 的なクラスを用意して、固定の値と RemoteConfig とで切り替えられるようにする
     [Serializable]
-    public class DownloadableProjectContext : ProjectContext, IDownloadableProjectContext {
+    [PublicAPI]
+    public class DownloadableProjectContext : ProjectContext, IDownloadableProjectContext
+    {
+        [SerializeField] private int assetBundleSingleManifestVersion;
 
-        [SerializeField]
-        private int assetBundleSingleManifestVersion;
-
-        public int AssetBundleSingleManifestVersion {
-            get {
-                return this.assetBundleSingleManifestVersion;
-            }
+        public int AssetBundleSingleManifestVersion
+        {
+            get { return assetBundleSingleManifestVersion; }
         }
 
-        public string InitialSceneName { get; set; }
+        [SerializeField] private string initialSceneName;
+
+        public string InitialSceneName
+        {
+            get { return initialSceneName; }
+        }
 
         public IURLResolver AssetBundleURLResolverSingleManifest { get; set; }
 
         public IURLResolver AssetBundleURLResolver { get; set; }
-
     }
-
 }
